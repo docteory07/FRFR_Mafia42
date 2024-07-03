1af95ad05e191cbb322b12cc0de2f38509f8853c
YoungjinKim <talkingjuly@gmail.com>
2024-07-03 21:40:43 +0900

    Updates

--- src/app.js
+++ src/app.js
@@ -5,14 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
-const init_1 = __importDefault(require("./routes/init"));
+const init_1 = __importDefault(require("../routes/init"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8000;
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.set('views', path_1.default.join(__dirname, 'views'));
-app.use(express_1.default.static(path_1.default.join(__dirname, './assets')));
+app.use(express_1.default.static(path_1.default.join(__dirname, '../assets')));
 app.use('/', init_1.default);
 app.use((err, req, res, next) => {
     console.error(err.stack);
