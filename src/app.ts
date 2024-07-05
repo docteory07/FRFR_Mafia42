04f8208b6b19f9915306c4cac33ccc5b956bcc7c
YoungjinKim <talkingjuly@gmail.com>
2024-07-05 19:44:22 +0900

    views, assets move to src

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,8 @@ app.use(express.urlencoded({ extended: true }))
 
 app.set("view engine", "ejs")
 
-app.set('views', path.join(__dirname, '../views'));
-app.use(express.static(path.join(__dirname, '../assets')))
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '/assets')))
 
 app.use('/', router)
 
